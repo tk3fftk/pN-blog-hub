6e0c4974754da2680f88bb02d89654af960165a6
Hiroki Takatsuka <3445553+tk3fftk@users.noreply.github.com>
2024-09-23 19:01:38 +0900

    Add Members (#5)
    * update members
    
    * modify about
    
    * add favicon
    
    * update about

--- members.ts
+++ members.ts
@@ -2,10 +2,31 @@ import { Member } from "@src/types";
 
 export const members: Member[] = [
   {
+    id: "trocco_blog",
+    name: "TROCCO Blog",
+    role: "Blog",
+    bio: "TROCCO Blog",
+    avatarSrc: "/avatars/trocco.png",
+    sources: ["https://blog.trocco.io/feed"],
+    twitterUsername: "trocco_jp",
+    githubUsername: "trocco-io",
+    websiteUrl: "https://blog.trocco.io",
+  },
+  {
+    id: "pn_blog",
+    name: "primeNumber Blog",
+    role: "Blog",
+    bio: "primeNumber Blog",
+    avatarSrc: "/logo.png",
+    sources: ["https://note.primenumber.co.jp/rss"],
+    twitterUsername: "primeNumberinc",
+    websiteUrl: "https://note.primenumber.co.jp/",
+  },
+  {
     id: "tk3fftk",
     name: "Hiroki Takatsuka",
     role: "SRE",
-    bio: "Site Reliability Engineer.",
+    bio: "SRE",
     avatarSrc:
       "https://pbs.twimg.com/profile_images/1696407915555688448/wPADdvGj_400x400.jpg",
     sources: [
@@ -17,14 +38,145 @@ export const members: Member[] = [
     githubUsername: "tk3fftk",
   },
   {
-    id: "trocco_blog",
-    name: "TROCCO Blog",
-    role: "Blog",
-    bio: "TROCCO Blog",
-    avatarSrc: "/avatars/trocco.png",
-    sources: ["https://blog.trocco.io/feed"],
-    twitterUsername: "trocco_jp",
-    githubUsername: "trocco-io",
-    websiteUrl: "https://blog.trocco.io",
+    id: "gtnao",
+    name: "n.nakane",
+    role: "Software Engineer",
+    bio: "Software Engineer",
+    avatarSrc: "https://avatars.githubusercontent.com/u/25474324?v=4",
+    sources: [
+      "https://zenn.dev/gtnao/feed",
+      "https://note.com/gtnao/rss",
+      "https://qiita.com/gtnao0219/feed",
+    ],
+    twitterUsername: "gtnao",
+    githubUsername: "gtnao",
+  },
+  {
+    id: "lana2548",
+    name: "Takaki Ihara",
+    role: "Senior Data Engineer",
+    bio: "Senior Data Engineer",
+    avatarSrc:
+      "https://qiita-user-profile-images.imgix.net/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fqiita-image-store%2F0%2F333022%2F94bc0204fbe830d3b9bf586cdeef2c61b9f6b89c%2Fx_large.png%3F1704773198?ixlib=rb-4.0.0&auto=compress%2Cformat&lossless=0&w=128&s=f9472964e0de55c33eaf31a74960b045",
+    sources: [
+      "https://zenn.dev/lana2548/feed",
+      "https://qiita.com/Lana2548_t/feed",
+    ],
+    twitterUsername: "l4_nq_t__",
+    githubUsername: "lana2548",
+  },
+  {
+    id: "mshr_dec",
+    name: "MSHR-Dec",
+    role: "SRE",
+    bio: "SRE",
+    avatarSrc: "https://avatars.githubusercontent.com/u/47933359?v=4",
+    sources: [
+      "https://zenn.dev/mshr_dec/feed",
+      "https://qiita.com/MSHR-Dec/feed",
+    ],
+    githubUsername: "mshr_dec",
+  },
+  {
+    id: "kekekenta",
+    name: "Kenta Suzuki",
+    role: "CTO",
+    bio: "CTO",
+    avatarSrc:
+      "https://assets.st-note.com/production/uploads/images/80259131/profile_9761428b8553d153d986492377b348fd.jpeg?width=4000&height=4000&fit=bounds&format=jpg&quality=90",
+    sources: ["https://note.com/kekekenta/rss"],
+    twitterUsername: "kekekenta",
+    githubUsername: "kekekenta",
+  },
+  {
+    id: "mnmandahalf",
+    name: "mnmandahalf",
+    role: "Software Engineer",
+    bio: "Software Engineer",
+    avatarSrc: "https://avatars.githubusercontent.com/u/15196363?v=4",
+    sources: [
+      "https://zenn.dev/mnmandahalf/feed",
+      "https://note.com/mnmandahalf/rss",
+      "https://qiita.com/mnmandahalf/feed",
+    ],
+    twitterUsername: "mnmandahalf",
+    githubUsername: "mnmandahalf",
+  },
+  {
+    id: "d",
+    name: "SoySoySoyB",
+    role: "Data Engineer",
+    bio: "Data Engineer",
+    avatarSrc:
+      "https://pbs.twimg.com/profile_images/1226548905287938050/W4kTVp8F_400x400.jpg",
+    sources: [
+      "https://note.com/SoySoySoyB/rss",
+      "https://qiita.com/SoySoySoyB/feed",
+    ],
+    twitterUsername: "SoySoySoyB",
+  },
+  {
+    id: "wakama1994",
+    name: "wakama1994",
+    role: "Data Engineer",
+    bio: "Data Engineer",
+    avatarSrc: "https://avatars.githubusercontent.com/u/92314128?v=4",
+    sources: [
+      "https://note.com/Wakama1994/rss",
+      "https://qiita.com/wakama1994/feed",
+    ],
+    twitterUsername: "Wakama1994",
+  },
+  {
+    id: "katamotokosuke",
+    name: "katamotokosuke",
+    role: "Software Engineer",
+    bio: "Software Engineer",
+    avatarSrc:
+      "https://qiita-user-profile-images.imgix.net/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fqiita-image-store%2F0%2F155328%2Fa7dcdc64affdc6bedb1f2c56d58f5ae207c1bb36%2Flarge.png%3F1592628886?ixlib=rb-4.0.0&auto=compress%2Cformat&lossless=0&w=128&s=8e0b7f75788359ab8878d4941f495084",
+    sources: ["https://qiita.com/katamotokosuke/feed"],
+  },
+  {
+    id: "yam_dr",
+    name: "yamaken",
+    role: "CIO",
+    bio: "CIO",
+    avatarSrc:
+      "https://pbs.twimg.com/profile_images/1220366370736525313/PCZ9Arx6_400x400.jpg",
+    sources: [
+      "https://note.com/pn_yamaken/rss",
+      "https://qiita.com/yam_dr/feed",
+    ],
+    twitterUsername: "yam_dr",
+  },
+  {
+    id: "komit08",
+    name: "Takato Komiyama",
+    role: "Data Engineer",
+    bio: "Data Engineer",
+    avatarSrc:
+      "https://pbs.twimg.com/profile_images/1643833833651945472/_BqgDOyM_400x400.jpg",
+    sources: ["https://qiita.com/Komi9T/feed"],
+    twitterUsername: "komit08",
+  },
+  {
+    id: "knuna_t",
+    name: "Kana Kitagawa/たいがー🐯",
+    role: "Community Manager",
+    bio: "Community Manager",
+    avatarSrc:
+      "https://pbs.twimg.com/profile_images/1790757932969365505/fYytJNyS_400x400.jpg",
+    sources: ["https://note.com/tiger_pn/rss"],
+    twitterUsername: "knuna_t",
+  },
+  {
+    id: "imooo67",
+    name: "mai ishikawa",
+    role: "HR",
+    bio: "HR",
+    avatarSrc:
+      "https://assets.st-note.com/production/uploads/images/93147774/profile_8a0e14584bf6f2779cce0b7117323fc8.jpg?width=4000&height=4000&fit=bounds&format=jpg&quality=90",
+    sources: ["https://note.com/imooo67/rss"],
+    twitterUsername: "m37pN",
   },
 ];
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,18 +16,9 @@ const Page: NextPage = () => {
               <a href="https://github.com/catnose99/team-blog-hub">
                 Team Blog Hub
               </a>
-              のデモです。ブログのRSSのURLを登録することで、チームメンバーの投稿を一覧にまとめて表示します。
+              を利用して作成された primeNumber Inc. の非公式ファンサイトです。
             </p>
-            <p>
-              Medium、note、Zenn、Qiita、はてなブログなど、RSSフィードを取得できるサイトであれば、メンバーは好きな場所に投稿できます。
-            </p>
-            <p>
-              詳しくは
-              <a href="https://zenn.dev/catnose99/articles/cb72a73368a547756862">
-                チーム個々人のテックブログをRSSで集約するサイトをNext.jsで作った
-              </a>
-              をご覧ください。
-            </p>
+            <p>何かあれば GitHub から Issue を作成してください。</p>
           </div>
           <div className="about__actions">
             <LinkBackHome />
