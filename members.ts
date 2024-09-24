5071a3381e0869760e7db0834aa377d3cad05b53
Hiroki Takatsuka <3445553+tk3fftk@users.noreply.github.com>
2024-09-24 09:43:36 +0900

    No role (#12)
    * no role, bio
    
    * remain role for blog

--- members.ts
+++ members.ts
@@ -5,7 +5,7 @@ export const members: Member[] = [
     id: "trocco_blog",
     name: "TROCCO Blog",
     role: "Blog",
-    bio: "TROCCO Blog",
+    bio: "Blog",
     avatarSrc: "/avatars/trocco.png",
     sources: ["https://blog.trocco.io/feed"],
     twitterUsername: "trocco_jp",
@@ -16,7 +16,7 @@ export const members: Member[] = [
     id: "pn_blog",
     name: "primeNumber Blog",
     role: "Blog",
-    bio: "primeNumber Blog",
+    bio: "Blog",
     avatarSrc: "/logo.png",
     sources: ["https://note.primenumber.co.jp/rss"],
     twitterUsername: "primeNumberinc",
@@ -25,8 +25,8 @@ export const members: Member[] = [
   {
     id: "tk3fftk",
     name: "Hiroki Takatsuka",
-    role: "SRE",
-    bio: "SRE",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://pbs.twimg.com/profile_images/1696407915555688448/wPADdvGj_400x400.jpg",
     sources: [
@@ -41,8 +41,8 @@ export const members: Member[] = [
   {
     id: "gtnao",
     name: "n.nakane",
-    role: "Software Engineer",
-    bio: "Software Engineer",
+    role: "",
+    bio: "",
     avatarSrc: "https://avatars.githubusercontent.com/u/25474324?v=4",
     sources: [
       "https://zenn.dev/gtnao/feed",
@@ -55,8 +55,8 @@ export const members: Member[] = [
   {
     id: "lana2548",
     name: "Takaki Ihara",
-    role: "Senior Data Engineer",
-    bio: "Senior Data Engineer",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://qiita-user-profile-images.imgix.net/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fqiita-image-store%2F0%2F333022%2F94bc0204fbe830d3b9bf586cdeef2c61b9f6b89c%2Fx_large.png%3F1704773198?ixlib=rb-4.0.0&auto=compress%2Cformat&lossless=0&w=128&s=f9472964e0de55c33eaf31a74960b045",
     sources: [
@@ -69,8 +69,8 @@ export const members: Member[] = [
   {
     id: "mshr_dec",
     name: "MSHR-Dec",
-    role: "SRE",
-    bio: "SRE",
+    role: "",
+    bio: "",
     avatarSrc: "https://avatars.githubusercontent.com/u/47933359?v=4",
     sources: [
       "https://zenn.dev/mshr_dec/feed",
@@ -81,8 +81,8 @@ export const members: Member[] = [
   {
     id: "kekekenta",
     name: "Kenta Suzuki",
-    role: "CTO",
-    bio: "CTO",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://assets.st-note.com/production/uploads/images/80259131/profile_9761428b8553d153d986492377b348fd.jpeg?width=4000&height=4000&fit=bounds&format=jpg&quality=90",
     sources: ["https://note.com/kekekenta/rss"],
@@ -92,8 +92,8 @@ export const members: Member[] = [
   {
     id: "mnmandahalf",
     name: "mnmandahalf",
-    role: "Software Engineer",
-    bio: "Software Engineer",
+    role: "",
+    bio: "",
     avatarSrc: "https://avatars.githubusercontent.com/u/15196363?v=4",
     sources: [
       "https://zenn.dev/mnmandahalf/feed",
@@ -106,8 +106,8 @@ export const members: Member[] = [
   {
     id: "d",
     name: "SoySoySoyB",
-    role: "Data Engineer",
-    bio: "Data Engineer",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://pbs.twimg.com/profile_images/1226548905287938050/W4kTVp8F_400x400.jpg",
     sources: [
@@ -119,8 +119,8 @@ export const members: Member[] = [
   {
     id: "wakama1994",
     name: "wakama1994",
-    role: "Data Engineer",
-    bio: "Data Engineer",
+    role: "",
+    bio: "",
     avatarSrc: "https://avatars.githubusercontent.com/u/92314128?v=4",
     sources: [
       "https://note.com/Wakama1994/rss",
@@ -131,8 +131,8 @@ export const members: Member[] = [
   {
     id: "katamotokosuke",
     name: "katamotokosuke",
-    role: "Software Engineer",
-    bio: "Software Engineer",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://qiita-user-profile-images.imgix.net/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fqiita-image-store%2F0%2F155328%2Fa7dcdc64affdc6bedb1f2c56d58f5ae207c1bb36%2Flarge.png%3F1592628886?ixlib=rb-4.0.0&auto=compress%2Cformat&lossless=0&w=128&s=8e0b7f75788359ab8878d4941f495084",
     sources: ["https://qiita.com/katamotokosuke/feed"],
@@ -140,8 +140,8 @@ export const members: Member[] = [
   {
     id: "yam_dr",
     name: "yamaken",
-    role: "CIO",
-    bio: "CIO",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://pbs.twimg.com/profile_images/1220366370736525313/PCZ9Arx6_400x400.jpg",
     sources: [
@@ -153,8 +153,8 @@ export const members: Member[] = [
   {
     id: "komit08",
     name: "Takato Komiyama",
-    role: "Data Engineer",
-    bio: "Data Engineer",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://pbs.twimg.com/profile_images/1643833833651945472/_BqgDOyM_400x400.jpg",
     sources: ["https://qiita.com/Komi9T/feed"],
@@ -163,8 +163,8 @@ export const members: Member[] = [
   {
     id: "knuna_t",
     name: "Kana Kitagawa/たいがー🐯",
-    role: "Community Manager",
-    bio: "Community Manager",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://pbs.twimg.com/profile_images/1790757932969365505/fYytJNyS_400x400.jpg",
     sources: ["https://note.com/tiger_pn/rss"],
@@ -173,8 +173,8 @@ export const members: Member[] = [
   {
     id: "imooo67",
     name: "mai ishikawa",
-    role: "HR",
-    bio: "HR",
+    role: "",
+    bio: "",
     avatarSrc:
       "https://assets.st-note.com/production/uploads/images/93147774/profile_8a0e14584bf6f2779cce0b7117323fc8.jpg?width=4000&height=4000&fit=bounds&format=jpg&quality=90",
     sources: ["https://note.com/imooo67/rss"],
