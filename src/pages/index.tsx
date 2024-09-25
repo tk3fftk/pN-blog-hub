60e9a67782a4434608ac7da0c6eb308f60bd9c0d
tk3fftk <tktk.stereoman@gmail.com>
2024-09-25 22:53:22 +0900

    remove unused

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import { NextPage } from "next";
-import Link from "next/link";
 
 import posts from "@.contents/posts.json";
 import { config } from "@site.config";
 import { PostItem } from "@src/types";
-import { ScrollableMembers } from "@src/components/ScrollableMembers";
 import { PostList } from "@src/components/PostList";
 import { PageSEO } from "@src/components/PageSEO";
-import {
-  ContentWrapper,
-  UndoWrapForScroll,
-} from "@src/components/ContentWrapper";
+import { ContentWrapper } from "@src/components/ContentWrapper";
 
 const Page: NextPage = () => {
   return (
